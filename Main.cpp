d1df81858c23ec74c0e623c84295eabdbb0f7661
marc-vinici <marc_vinici@hotmail.com>
2025-01-09 20:11:18 -0300

    Add ConnectionProtocol test in Main function

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,23 @@
-#include<print>
+#include <print>
+
 #include "ConnectionProtocol.h"
 
-int main(){
-  std::println("Hello World");
+int main() {
+  std::println("Connection Protocol!");
+
   ConnectionProtocol connection;
+
+  auto events = std::vector<ConnectionProtocol::Event>{
+      ConnectionProtocol::Event::kInterfaceOk,
+      ConnectionProtocol::Event::kPacketOk,
+      ConnectionProtocol::Event::kPacketNok,
+      ConnectionProtocol::Event::kPacketOk,
+      ConnectionProtocol::Event::kPacketNotReceived,
+      ConnectionProtocol::Event::kInterfaceOk};
+
+  for (const auto& event : events) {
+    connection.processEvent(event);
+  }
+
   return 0;
 }
